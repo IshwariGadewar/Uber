9c6abc44be4539b67cd2ea1903d8d742e29c7862
IshwariGadewar <gadewareshwari24@gmail.com>
2025-03-07 01:25:13 +0530

    user authentication done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blacklistTokenSchema = require('../models/blacklistToken.model');
 
 
 module.exports.registerUser = async (req,res,next) => {
@@ -22,4 +23,39 @@ module.exports.registerUser = async (req,res,next) => {
     });
     const token = user.generateAuthTokens();
     res.status(201).json({token,user});
+}
+
+module.exports.loginUser = async (req,res,next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    const user = await userModel.findOne({email}).select('+password');
+    if(!user){
+        return res.status(401).json({message: 'INVALID EMAIL OR PASSWORD'});
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({message: 'INVALID EMAIL OR PASSWORD'});
+    }
+
+    const token = user.generateAuthTokens();
+    res.cookie('token',token);
+    res.status(200).json({token,user});
+}
+
+module.exports.getUserProfile = async (req,res,next) => {
+    res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req,res,next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blacklistTokenSchema.create({token});
+
+    res.status(200).json({message: 'LOGGED OUT SUCCESSFULLY'});
 }
